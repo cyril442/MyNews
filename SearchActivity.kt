70ff79fbe2aa50ea072077c36d3840aa134eb929
Cyril <37065900+cyril442@users.noreply.github.com>
2019-03-20 17:50:24 +0100

    On process to link CalendarFragm to SearchActivity

--- SearchActivity.kt
+++ SearchActivity.kt
@@ -1,18 +1,21 @@
 package com.example.mynews
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.widget.Toolbar
+import com.example.mynews.Fragments_Search.CalendarFragment
 
-class SearchActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(v: View?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class SearchActivity : AppCompatActivity(), View.OnClickListener, CalendarFragment.CalendarFragmentListener {
 
 
+    lateinit var calendarFragment : CalendarFragment
+
+
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -21,11 +24,38 @@ class SearchActivity : AppCompatActivity(), View.OnClickListener {
         setSupportActionBar(toolbar)
         supportActionBar!!. setDisplayHomeAsUpEnabled(true)
 
+        // Recuperation of the Fragment into the Search Activity
+        calendarFragment = CalendarFragment()
+
+
+
+        // the FindViewById from the Fragments collected into the Search Activity for Treatment before request HTTP to the server
+        val editTextForSearch = findViewById<EditText>(R.id.edit_query)
         val button = findViewById<Button>(R.id.button_search)
 
+
+
+
+
+
+        // OnClick of the button Search
         button.setOnClickListener(View.OnClickListener {
+            val querytext = editTextForSearch.text
+            var begin = calendarFragment.entryDate
+
             Toast.makeText(this, "Search Button Clicked", Toast.LENGTH_SHORT).show()
+            Log.i("Texte affiché ", " $querytext")
+            Log.i("BEG", " $begin ")
+
+
         })
 
     }
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    override fun onDateBegin(entryDate: String) {
+
+     }
+
 }
